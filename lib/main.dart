c08ac9cc371bf5360b0fced70b82ac71ccc65a45
Salonee Shrestha <saloneeshrestha35@gmail.com>
2026-07-15 13:47:28 +0545

    added approval screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nibangsh_consultancy/admin/approvals/views/approvals_screen.dart';
 import 'package:nibangsh_consultancy/src/academic_details/views/academic_details_screen.dart';
-import 'package:nibangsh_consultancy/admin/applications/views/applications_screen.dart';
 import 'package:nibangsh_consultancy/admin/nav/admin_navigation.dart';
 import 'package:nibangsh_consultancy/admin/notifications/views/notifications_screen.dart';
 import 'package:nibangsh_consultancy/admin/settings/views/settings_screen.dart';
@@ -66,8 +66,8 @@ class MyApp extends StatelessWidget {
         ),
         GetPage(name: AppRoute.students, page: () => const StudentsScreen()),
         GetPage(
-          name: AppRoute.applications,
-          page: () => const ApplicationsScreen(),
+          name: AppRoute.approvals,
+          page: () => const ApprovalsScreen(),
         ),
         GetPage(name: AppRoute.settings, page: () => const SettingsScreen()),
         GetPage(
