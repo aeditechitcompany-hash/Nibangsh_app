b250c1ed01640b158ec2ee89946e67612e063273
Salonee Shrestha <saloneeshrestha35@gmail.com>
2026-07-23 18:20:18 +0545

    added mcq and changed docs

---
+++ lib/src/mcq/views/mcq_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class McqScreen extends StatefulWidget {
+  const McqScreen({super.key});
+
+  @override
+  State<McqScreen> createState() => _McqScreenState();
+}
+
+class _McqScreenState extends State<McqScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
