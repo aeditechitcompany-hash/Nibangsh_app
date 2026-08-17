86468f33aec17fa79c1b442be323d2f8df52e8fa
Swosti Makaju <swosti.makaju@gmail.com>
2026-08-17 18:01:46 +0545

    fixed image

--- lib/common/api_services/api_constants.dart
+++ lib/common/api_services/api_constants.dart
@@ -2,10 +2,10 @@ class ApiConstants {
   // BASE URL
 
   static const String baseUrl =
-      'http://192.168.101.29:8000/api';
+      'http://192.168.1.92:8000/api';
 
   static const String serverUrl =
-      'http://192.168.101.29:8000';
+      'http://192.168.1.92:8000';
 
   // AUTH
 
